17abfe65c432d93aad3727812ba1734736baa583
Benjamin Clarke <benjaminclarke@eduroam-252-200.nomadic.bris.ac.uk>
2015-06-19 16:57:03 +0100

    some seq semantics working

--- translation_tool/Parser.py
+++ translation_tool/Parser.py
@@ -35,7 +35,7 @@ class Semantic_analyser(object):
     def call_stack(self):
         return self._call_stack
 
-    def is_already_declared(self, ID):
+    def is_declared(self, ID):
         """Returns False if ID has not been declared yet"""
         if self.sym_table.id_in(self.call_stack.peek(), ID[0]) == True:  # NOQA
             return True
@@ -43,25 +43,23 @@ class Semantic_analyser(object):
             return False
 
     def analyse_id_set(self, tokens):
-        token = tokens[0]
-        if self.is_already_declared(token("ID")) is True:
-            raise ParseException("Unknown Value")
-        if self.value_is_correct_type(token("ID"), token("value")) is False:
+        if self.is_declared(tokens[0]["target"]) is False:
+            raise ParseException("Undeclared Symbol")
+        if self.value_is_correct_type(self.sym_table.id_type(self.call_stack.peek(), tokens[0]["target"][0]), tokens[0]["value"][0]) is False:
                 raise ParseException("Incorrect Value Type assigned to ID")
 
-    def value_is_correct_type(self, ID, value):
-        id_type = self.sym_table.id_type(self.call_stack.peek(), ID)
+    def value_is_correct_type(self, id_type, value):
         if id_type is "Bit" and self.is_bit(value) is True:
             return True
         if id_type is "Int" and self.is_int(value) is True:
             return True
-        print("Incorrect Value assigned to type")
+        if id_type is "Seq" and self.is_seq(value) is True:
+            return True
         return False
 
     def is_int(self, value):
         try:
             self.convert_base(value)
-            # int(value)
         except ValueError:
             print("Cannot assign non-integer to Int")
             return False
@@ -96,6 +94,30 @@ class Semantic_analyser(object):
         else:
             return True
 
+    def analyse_seq_decl(self, tokens):
+        # print(tokens[0].dump())
+        if self.is_declared(tokens[0]["ID"]) is True:
+            raise ParseException("Duplicate Symbol")
+        try:
+            if self.seq_correct_type(tokens[0]["decl"]["type"], tokens[0]["value"], len(tokens[0]['seq_size'])) is False:
+                raise ParseException("Sequence Value Error")
+        except KeyError:
+            pass
+        return tokens
+
+    def seq_correct_type(self, expected_type, sequence, depth):
+        if(depth < 0):
+            return False
+        depth -= 1
+        for ele in sequence:
+            if type(ele) == str:
+                if self.value_is_correct_type(expected_type, ele) is False or depth > 0:
+                    raise ParseException("Incorect Value For sequence")
+                    return False
+            else:
+                return self.seq_correct_type(expected_type, ele, depth)
+            return True
+
     def analyse_int_decl(self, tokens):
         token = tokens[0]
         for decl in token["value"]:
@@ -105,7 +127,7 @@ class Semantic_analyser(object):
                     raise ParseException("Value bigger than bit constraints")
             except KeyError:
                 pass
-            if self.is_already_declared(decl["ID"]) is True:
+            if self.is_declared(decl["ID"]) is True:
                     raise ParseException("Duplicate Symbol")
             self.add_int_decl(decl, token)
         return tokens
@@ -154,14 +176,16 @@ class Parser(object):
         self.false_ = Keyword('False')
         self.true_ = Keyword('True')
         self.bit_ = Keyword('Bit')
+        self.sbox_ = Keyword('sbox')
         self.l_shift_ = Keyword('<<')
         self.r_shift_ = Keyword('>>')
         self.circ_l_shift_ = Keyword('<<<')
         self.circ_r_shift_ = Keyword('>>>')
         self.bit_val = self.false_ ^ self.true_
-        self.ID = NotAny(self.int_ ^ self.bit_) + Word(alphas + '_', alphanums + '_')  # NOQA
         self.if_ = Keyword('if')
         self.for_ = Keyword('for')
+        self.return_ = Keyword('return')
+        self.ID = NotAny(self.int_ ^ self.bit_ ^ self.if_ ^ self.for_ ^ self.sbox_) + Word(alphas + '_', alphanums + '_')  # NOQA
         # Other Tokens
         self.l_bracket = Literal('(')
         self.r_bracket = Literal(')')
@@ -204,37 +228,49 @@ class Parser(object):
         self.expr = Forward()
         self.seq_val = Forward()
         self.cast = Forward()
+
         self.int_value = self.b_2_num ^ self.b_16_num ^ self.b_10_num
 
-        self.seq = (self.int_ ^ self.bit_)("type") + Group(OneOrMore(~White() + Suppress(self.l_sq_b) + Word(nums) + Suppress(self.r_sq_b)))("seq_size")
+        self.seq_ = (self.int_ ^ self.bit_)("type") + Group(OneOrMore(~White() + Suppress(self.l_sq_b) + Word(nums) + Suppress(self.r_sq_b)))("seq_size")
 
-        self.seq_val << Group(Suppress(self.l_bracket) + (delimitedList(self.int_value) ^
-                                                          delimitedList(self.bit_val) ^
-                                                          delimitedList(self.seq_val)) +
+        self.seq_val << Group(Suppress(self.l_bracket) + (delimitedList(self.int_value)('int') ^
+                                                          delimitedList(self.bit_val)('bit') ^
+                                                          delimitedList(self.seq_val)('seq')) +
                               Suppress(self.r_bracket))
+
         self.seq_range = Group(Word(nums) + Literal(":") + Word(nums))
+
         self.seq_index_select = Group(self.ID("ID") + ~White() +
                                       Suppress(self.l_sq_b) + delimitedList(Word(nums) ^ self.seq_range)("index") + Suppress(self.r_sq_b))
+
+        self.sbox_call = Group((self.ID ^ self.seq_val) + ~White() + Literal(".") + ~White() + self.sbox_ + ~White() +
+                               self.l_bracket + (self.ID ^ self.int_value) + self.r_bracket)
 
         self.var = self.ID ^ self.int_value ^ self.bit_val ^ self.seq_val ^ self.seq_index_select ^ self.cast
 
         self.id_set = Group((self.ID ^ self.seq_index_select)("target") + self.eq_set + (self.expr)("value"))
+        self.id_set.setParseAction(self.semantic_analyser.analyse_id_set)  # NOQA # Remeber ID must be ID[0]!
 
-        # id_set.setParseAction(semantic_analyser.analyse_id_set)  # NOQA # Remeber ID must be ID[0]!
-
-        self.int_decl = Group(self.int_("decl") + Optional(Suppress(self.l_bracket) + Word(nums)("constraints") +
+        self.int_decl = Group(self.int_("decl") + Optional(~White() + Suppress(self.l_bracket) + Word(nums)("constraints") +
             Suppress(self.r_bracket)) + delimitedList(Group(self.ID("ID")) ^ Group(self.ID("ID") + self.eq_set + self.int_value("value")))("value"))  # NOQA
         self.int_decl.setParseAction(self.semantic_analyser.analyse_int_decl)
 
         self.bit_decl = Group(self.bit_("decl") + delimitedList(Group(self.ID) ^ Group(self.ID + self.eq_set + self.bit_val("value")))("value"))
 
-        self.seq_decl = Group(self.seq + self.ID + Optional(self.eq_set + self.seq_val("value")))
+        self.seq_decl = Group(self.seq_("decl") + self.ID("ID") + Optional(self.eq_set + self.seq_val("value")))
+        self.seq_decl.setParseAction(self.semantic_analyser.analyse_seq_decl)
 
         self.comparison = infixNotation(infixNotation(self.var,  # NOQA
                                                            [(self.comparison_op, 2, opAssoc.LEFT), ]),
                                                            [(self.log_op, 2, opAssoc.LEFT), ])
 
         self.decl = self.bit_decl ^ self.int_decl ^ self.seq_decl
+
+        self.return_stmt = Group(self.return_ + self.var)
+
+        self.function_decl = Group((self.int_ ^ self.bit_ ^ self.seq_)("return_type") + self.ID +
+                                   Suppress(self.l_bracket) + Group(delimitedList((self.seq_ ^ self.int_ ^ self.bit_) + self.ID))("func_param") +
+                                   Suppress(self.r_bracket) + Suppress(self.l_brace) + Group(self.stmt)("body") + Suppress(self.r_brace))
 
         self.expr << Group(infixNotation(self.var, [(self.bin_op("Operator"), 2, opAssoc.LEFT), ]))
 
@@ -245,17 +281,38 @@ class Parser(object):
 
         self.if_stmt = Group(self.if_ + self.l_bracket + self.comparison + self.r_bracket + self.l_brace + self.stmt + self.r_brace)
 
-        self.cast << Suppress(self.l_bracket) + (self.int_ ^ self.bit_ ^ self.seq)("dest_type") + Suppress(self.r_bracket) + self.var("target")
+        self.cast << Group(Suppress(self.l_bracket) + (self.int_ ^ self.bit_ ^ self.seq_)("dest_type") + Suppress(self.r_bracket) + self.var("target"))
+
+        self.function_call = Group(self.ID + ~White() + Suppress(self.l_bracket) + delimitedList(self.var)("param_list") + Suppress(self.r_bracket))
 
         self.stmt << ZeroOrMore(self.decl + Suppress(self.term_st)
                                 | self.id_set + Suppress(self.term_st)
                                 | self.expr + Suppress(self.term_st)
                                 | self.for_loop
-                                | self.if_stmt)
+                                | self.if_stmt
+                                | self.return_stmt + Suppress(self.term_st)
+                                | self.function_call + Suppress(self.term_st)
+                                | self.sbox_call + Suppress(self.term_st))
 
-        self.grammar = self.stmt + StringEnd()
+        self.grammar_test = self.stmt + StringEnd()  # Allows single statements to be parsed
+
+        self.grammar = self.function_decl + StringEnd()
 
     def parse_test_unit(self, data_in):
+        """Parses single statements"""
+        try:
+            res = self.grammar_test.parseString(data_in)
+        except ParseException as details:
+            print("The following error occured:")
+            print(details)
+            return False
+        if type(res[0]) is not bool:
+            pass
+            # print(res[0].dump())
+        return [res, True]
+
+    def parse_test_integration(self, data_in):
+        """Only Parses Statements in functions"""
         try:
             res = self.grammar.parseString(data_in)
         except ParseException as details:
@@ -265,14 +322,6 @@ class Parser(object):
         if type(res[0]) is not bool:
             print(res[0].dump())
         return [res, True]
-
-    def parse_test(self, data_in):
-        try:
-            res = self.grammar.parseString(data_in)
-        except ParseException:
-            # print(detail)
-            pass
-        return res
 
     def parse(self, data_in):
         return self.grammar.parseString(data_in)
@@ -306,6 +355,7 @@ class TestParser(unittest.TestCase):
     #     self.assertEqual(par.parse_test_unit("Int(10) r1 = 10, s1, t1 = 5, u1; Int v1, w1=10, \
     #         ll1;")[1], True)
     #     self.assertEqual(par.parse_test_unit("Int dd = 10;")[1], True)
+    #     self.assertEqual(par.parse_test_unit("Int (123) zz;"), False)
 
     # def test_int_decl_semantics(self):
     #     sem_a = Semantic_analyser()
@@ -448,20 +498,59 @@ class TestParser(unittest.TestCase):
     #     self.assertEqual(par.parse_test_unit("a[5,6,7-8:10];"), False)
     #     self.assertEqual(par.parse_test_unit("a[5,6,7,8:10]; b[5,6,7,8:10];")[1], True)
 
-    def test_cast_parsing(self):
+    # def test_cast_parsing(self):
+    #     par = Parser()
+    #     self.assertEqual(par.parse_test_unit("(Int) a;")[1], True)
+    #     self.assertEqual(par.parse_test_unit("(Bit) a;")[1], True)
+    #     self.assertEqual(par.parse_test_unit("(Int[4]) 10;")[1], True)
+    #     self.assertEqual(par.parse_test_unit("(Int[4]) Bit[10];"), False)
+    #     self.assertEqual(par.parse_test_unit("(Int[4]) Int;"), False)
+    #     self.assertEqual(par.parse_test_unit("(Int[4]) Bit;"), False)
+
+    # def test_function_call_parsing(self):
+    #     par = Parser()
+    #     self.assertEqual(par.parse_test_unit("func(a, b, 10, 40);")[1], True)
+    #     self.assertEqual(par.parse_test_unit("func(a, b, Int[10] a, 40);"), False)
+    #     self.assertEqual(par.parse_test_unit("func(a, b, (1,0,1,1), 40);")[1], True)
+    #     self.assertEqual(par.parse_test_unit("func (a, b, (1,0,1,1), 40);"), False)
+
+    # def sbox_call_parsing(self):
+    #     par = Parser()
+    #     self.assertEqual(par.parse_test_unit("a.sbox(1);")[1], True)
+    #     self.assertEqual(par.parse_test_unit("a.sbox(a);")[1], True)
+    #     self.assertEqual(par.parse_test_unit("a. sbox(a);"), False)
+    #     self.assertEqual(par.parse_test_unit("a .sbox(a);"), False)
+    #     self.assertEqual(par.parse_test_unit("a.sbo x(a);"), False)
+    #     self.assertEqual(par.parse_test_unit("a.sbox (a);"), False)
+
+    # def test_function_parsing(self):
+    #     par = Parser()
+    #     self.assertEqual(par.parse_test_integration("Int testFunction(Int a, Bit b, Int[63] c, Int[1][2][3] d) {\
+    #         a = 100;\
+    #         return a;\
+    #         }")[1], True)
+    #     self.assertEqual(par.parse_test_integration("Int a;"), False)
+
+    def test_seq_semantics(self):
         par = Parser()
-        self.assertEqual(par.parse_test_unit("(Int) a;")[1], True)
-        self.assertEqual(par.parse_test_unit("(Bit) a;")[1], True)
-        self.assertEqual(par.parse_test_unit("(Int[4]) 10;")[1], True)
-        self.assertEqual(par.parse_test_unit("(Int[4]) Bit[10];"), False)
+        self.assertEqual(par.semantic_analyser.seq_correct_type("Int", ["1", "2", "3", "4"], 1), True)
+        self.assertEqual(par.semantic_analyser.seq_correct_type("Int", [["1", "2", "3", "4"]], 2), True)
+
+    def test_function_id_set_semantics(self):
+        par = Parser()
+        par.parse_test_unit("Int a;")
+        self.assertEqual(par.parse_test_unit("a = 1;")[1], True)
+        self.assertEqual(par.parse_test_unit("b = 1;"), False)
+        par.parse_test_unit("Int b;")
+        self.assertEqual(par.parse_test_unit("b = 1;")[1], True)
+        par.parse_test_unit("Int[10][10] c;")
+        self.assertEqual(par.parse_test_unit("Int[10] d = (1,2,3,4);")[1], True)
+        self.assertEqual(par.parse_test_unit("Int[1][2] e = ((1,2),(1,2),(1,2),(1,2),(1,2),(1,2),(1,2),(1,2));")[1], True)
+        self.assertEqual(par.parse_test_unit("Int[1][2][4] e = (((1,2),(1,2),(1,2),(1,2)),((1,2),(1,2),(1,2),(1,2)));")[1], True)
+        self.assertEqual(par.parse_test_unit("Int[1][2] e = (((1,2),(1,2),(1,2),(1,2)),((1,2),(1,2),(1,2),(1,2)));"), False)
+        self.assertEqual(par.parse_test_unit("Int[1][2] e = ((((1,2),(1,2),(1,2),(1,2)),((1,2),(1,2),(1,2),(1,2))));"), False)
+        self.assertEqual(par.parse_test_unit("Int[1][2][4] e = (((True,False),(True,False),(True,False),(True,False)),((True,False),(True,False),(True,False),(True,False)));"), False)  # NOQA
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(TestParser)
     unittest.TextTestRunner(verbosity=2).run(suite)
-
-
-
-
-
-
-
